01ee53e7ecd3f631f4ceb0a68f73aa02a07e0cc9
edwin xiang <edwinxiang@outlook.com>
2022-06-01 15:37:44 +0800

    First commit, on 2022-06-01

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+ZetaBase goals:
+1. Web3.0 based
+2. Support three scenarios: R&D(including Biotech), Marketing, Finance
+3. Cloud and SaaS based
+4. Protect data security and personal privacy
+5. Support ZB scale data
+6. Distributed
+7. Block-Chain Based
+8. Support rapid data analysis and data warehouse
+9. Support data exchange
+10. Support build-in AI models, low-code and self-service analytics
+ */
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to ZetaBase world, hope you have a wonderful journey here!")
+}
